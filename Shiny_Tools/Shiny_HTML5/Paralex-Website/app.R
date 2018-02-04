ecf92ca9e96893b2b7abbced68839ecf343ecc78
DannyJRa <danny_zurich@outlook.com>
2018-02-04 23:08:46 +0100

    HTML5

---
+++ Shiny_Tools/Shiny_HTML5/Paralex-Website/app.R
@@ -0,0 +1,24 @@
+ #
+# Define server logic for random distribution app ----
+server <- function(input, output) {
+  
+  # Reactive expression to generate the requested distribution ----
+  # This is called whenever the inputs change. The output functions
+  # defined below then use the value computed from this expression
+
+  
+  # Generate a plot of the data ----
+  # Also uses the inputs to build the plot label. Note that the
+  # dependencies on the inputs and the data reactive expression are
+  # both tracked, and all expressions are called in the sequence
+  # implied by the dependency graph.
+  output$plot3 <- renderPlot({
+
+    plot(rnorm(100))
+  })
+  
+
+  
+}
+
+shinyApp(ui = htmlTemplate("www/index.html"), server)
